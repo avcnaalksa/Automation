1d1e23118938c447b839279800362e2a971c83c6
avcnaalksa <its.me.avicena@gmail.com>
2024-11-26 12:07:06 +0700

    Update mikrotik.sh

--- mikrotik.sh
+++ mikrotik.sh
@@ -1,53 +1,23 @@
-#!/bin/sh
+#!/bin/bash
 
-apt install expect -y
-apt install telnet 
+# Konfigurasi MikroTik via SSH
+MKT_HOST="192.168.6.3"  # IP MikroTik sesuai topologi
+MKT_USER="admin"        # Default username MikroTik (tanpa password)
 
-MIKROTIK_USER="admin"
-MIKROTIK_PASS="123"
-MIKROTIK_IP="192.168.6.10"
+# Instal sshpass jika belum ada
+sudo apt install sshpass -y
 
-expect << EOF
-spawn telnet $MIKROTIK_IP
-expect "login:"
-send "$MIKROTIK_USER\r"
-expect "Password:"
-send "$MIKROTIK_PASS\r"
-expect ">"
-
-# Menambahkan DHCP Client di ether1
-send "/interface dhcp-client add interface=ether1 disabled=no\r"
-expect ">"
-
-# Menambahkan IP address di ether2
-send "/ip address add address=192.168.200.1/24 interface=ether2\r"
-expect ">"
-
-# Membuat IP pool
-send "/ip pool add name=dhcp_pool ranges=192.168.200.10-192.168.200.100\r"
-expect ">"
-
-# Menambahkan DHCP Server
-send "/ip dhcp-server add name=dhcp1 interface=ether2 address-pool=dhcp_pool disabled=no\r"
-expect ">"
-
-# Menambahkan konfigurasi network DHCP Server
-send "/ip dhcp-server network add address=192.168.200.0/24 gateway=192.168.200.1\r"
-expect ">"
-# Menambahkan konfigurasi network DHCP Server
-send "/ip dhcp-server enable dhcp1"
-expect ">"
-
-# Menambahkan static route ke Ubuntu Server
-send "/ip route add gateway=192.168.6.1\r"
-expect ">"
-
-# Menambahkan aturan firewall NAT untuk internet sharing
-send "/ip firewall nat add chain=srcnat out-interface=ether1 action=masquerade\r"
-expect ">"
-
-
-# Keluar dari MikroTik
-send "exit\r"
-expect eof
+# Eksekusi perintah MikroTik
+echo "Menghubungkan ke MikroTik dan melakukan konfigurasi..."
+sshpass -p "" ssh -o StrictHostKeyChecking=no $MKT_USER@$MKT_HOST << EOF
+/interface vlan
+add interface=ether1 name=vlan10 vlan-id=10
+/ip address
+add address=192.168.6.254/24 interface=vlan10
+/ip dhcp-client
+add interface=ether2 disabled=no
+/ip firewall nat
+add chain=srcnat out-interface=ether2 action=masquerade
 EOF
+
+echo "Konfigurasi MikroTik selesai!"
