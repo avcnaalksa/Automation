aa4e6ad242aca66341a560ba1cc6cdddf4effc30
avcnaalksa <its.me.avicena@gmail.com>
2024-11-26 07:35:22 +0700

    Update and rename mikrotik_config.rsc to mikrotik_config.sh

--- mikrotik_config.rsc
+++
@@ -1,9 +0,0 @@
-# Script Otomatisasi
-/system script add name=autoConfig source={
-    /ip route add dst-address=192.168.6.50/0 gateway=192.168.6.1
-    /ip dhcp-client add interface=eth2 disabled=no
-}
-
-# Jadwal Script Biar Otomatis Waktu Startup
-/system scheduler add name=runAutoConfig on-event=autoConfig start-time=startup
-
---
+++ mikrotik_config.sh
@@ -0,0 +1,53 @@
+#!/bin/sh
+
+apt install expect -y
+apt install telnet 
+
+MIKROTIK_USER="admin"
+MIKROTIK_PASS="123"
+MIKROTIK_IP="192.168.6.10"
+
+expect << EOF
+spawn telnet $MIKROTIK_IP
+expect "login:"
+send "$MIKROTIK_USER\r"
+expect "Password:"
+send "$MIKROTIK_PASS\r"
+expect ">"
+
+# Menambahkan DHCP Client di ether1
+send "/interface dhcp-client add interface=ether1 disabled=no\r"
+expect ">"
+
+# Menambahkan IP address di ether2
+send "/ip address add address=192.168.200.1/24 interface=ether2\r"
+expect ">"
+
+# Membuat IP pool
+send "/ip pool add name=dhcp_pool ranges=192.168.200.10-192.168.200.100\r"
+expect ">"
+
+# Menambahkan DHCP Server
+send "/ip dhcp-server add name=dhcp1 interface=ether2 address-pool=dhcp_pool disabled=no\r"
+expect ">"
+
+# Menambahkan konfigurasi network DHCP Server
+send "/ip dhcp-server network add address=192.168.200.0/24 gateway=192.168.200.1\r"
+expect ">"
+# Menambahkan konfigurasi network DHCP Server
+send "/ip dhcp-server enable dhcp1"
+expect ">"
+
+# Menambahkan static route ke Ubuntu Server
+send "/ip route add gateway=192.168.6.1\r"
+expect ">"
+
+# Menambahkan aturan firewall NAT untuk internet sharing
+send "/ip firewall nat add chain=srcnat out-interface=ether1 action=masquerade\r"
+expect ">"
+
+
+# Keluar dari MikroTik
+send "exit\r"
+expect eof
+EOF
