53e9bb1b3a5e7e1f4bb0231902ebefd5b27bd9fe
avcnaalksa <its.me.avicena@gmail.com>
2024-11-26 07:36:57 +0700

    Update setupvincent.sh

--- setupvincent.sh
+++ setupvincent.sh
@@ -1,41 +1,9 @@
 #!/bin/bash
 
-# Variabel Konfigurasi
-VLAN_INTERFACE="eth1.10"
-VLAN_ID=10
-IP_ADDR="192.168.6.1/24"      # IP address untuk interface VLAN di Ubuntu
-DHCP_CONF="/etc/dhcp/dhcpd.conf"
-SWITCH_IP="192.168.6.35"       # IP Cisco Switch yang akan dikonfigurasi
-MIKROTIK_IP="192.168.200.1"     # IP MikroTik yang baru
-USER_SWITCH="root"              # Username SSH untuk Cisco Switch
-USER_MIKROTIK="admin"           # Username SSH default MikroTik
-PASSWORD_SWITCH="root"          # Password untuk Cisco Switch
-PASSWORD_MIKROTIK=""            # Kosongkan jika MikroTik tidak punya password
+# Otomasi Dimulai
+echo "Memulai Otomasi Ubuntu DHCP Server dan SSH"
 
-set -e
-
-# Warna untuk tampilan
-RED='\033[31m'
-GREEN='\033[32m'
-YELLOW='\033[33m'
-BLUE='\033[34m'
-CYAN='\033[36m'
-RESET='\033[0m'
-
-# Fungsi untuk menampilkan pesan sukses atau gagal
-print_status() {
-  if [ $? -eq 0 ]; then
-    echo -e "${GREEN}✔ Sukses${RESET}"
-  else
-    echo -e "${RED}✘ Gagal${RESET}"
-    exit 1
-  fi
-}
-
-echo -e "${CYAN}Skrip Otomasi Ubuntu dimulai. Siapkan sistem Anda untuk otomatisasi konfigurasi!${RESET}"
-
-# 1. Menambahkan Repositori Kartolo
-echo -e "${BLUE}Menambahkan repositori Kartolo ke sumber apt...${RESET}"
+# Repo Kartolo
 cat <<EOF | sudo tee /etc/apt/sources.list
 deb http://kartolo.sby.datautama.net.id/ubuntu/ focal main restricted universe multiverse
 deb http://kartolo.sby.datautama.net.id/ubuntu/ focal-updates main restricted universe multiverse
@@ -45,63 +13,65 @@ deb http://kartolo.sby.datautama.net.id/ubuntu/ focal-proposed main restricted u
 EOF
 
 sudo apt update
-sudo apt install sshpass -y
-sudo apt install -y isc-dhcp-server iptables iptables-persistent
 
-# 2. Menyiapkan dan Mengaktifkan Interface Ethernet
-echo -e "${YELLOW}Memeriksa dan mengaktifkan interface ethernet...${RESET}"
-ip link set eth0 up
-ip link set eth1 up
-print_status
-
-# 3. Konfigurasi VLAN di Ubuntu Server
-echo -e "${YELLOW}Membuat VLAN di Ubuntu Server...${RESET}"
-ip link add link eth1 name $VLAN_INTERFACE type vlan id $VLAN_ID
-ip addr add $IP_ADDR dev $VLAN_INTERFACE
-ip link set up dev $VLAN_INTERFACE
-print_status
-
-# 4. Konfigurasi DHCP Server
-echo -e "${CYAN}Mengonfigurasi DHCP Server...${RESET}"
-cat <<EOL | sudo tee $DHCP_CONF
-# Konfigurasi subnet untuk VLAN 10
-subnet 192.168.6.0 netmask 255.255.255.0 {
-    range 192.168.6.10 192.168.6.100;
-    option routers 192.168.6.1;
-    option subnet-mask 255.255.255.0;
-    option domain-name-servers 8.8.8.8, 8.8.4.4;
-    option domain-name "example.local";
-}
-EOL
-print_status
-
-# 5. Menyiapkan Konfigurasi Netplan
-echo -e "${CYAN}Menyusun konfigurasi Netplan...${RESET}"
-cat <<EOF | sudo tee /etc/netplan/01-netcfg.yaml
+# Konfigurasi Network (Netplan)
+cat <<EOT > /etc/netplan/01-netcfg.yaml
 network:
   version: 2
+  renderer: networkd
   ethernets:
     eth0:
-     dhcp4: true
+      dhcp4: yes
     eth1:
       dhcp4: no
   vlans:
-     eth1.10:
-       id: 10
-       link: eth1
-       addresses: [192.168.6.1/24]
+    eth1.10:
+      id: 10
+      link: eth1
+      addresses:
+        - 192.168.6.1/24
+EOT
+netplan apply
+
+# Instalasi ISC DHCP Server
+sudo apt install isc-dhcp-server -y
+
+# Konfigurasi DHCP Server
+cat <<EOF | sudo tee /etc/dhcp/dhcpd.conf
+subnet 192.168.6.0 netmask 255.255.255.0 {
+  range 192.168.6.2 192.168.6.254;
+  option domain-name-servers 8.8.8.8;
+  option subnet-mask 255.255.255.0;
+  option routers 192.168.6.1;
+  option broadcast-address 192.168.6.255;
+  default-lease-time 600;
+  max-lease-time 7200;
+}
 EOF
-print_status
 
-# 6. Menerapkan Konfigurasi Netplan
-echo -e "${CYAN}Menerapkan konfigurasi Netplan...${RESET}"
-sudo netplan apply
-print_status
-
-# 7. Restart DHCP server untuk menerapkan konfigurasi baru
-echo -e "${CYAN}Merestart DHCP server...${RESET}"
+# Konfigurasi ISC DHCP Server untuk VLAN
+echo 'INTERFACESv4="eth1.10"' | sudo tee /etc/default/isc-dhcp-server
 sudo systemctl restart isc-dhcp-server
-sudo systemctl status isc-dhcp-server
-print_status   
 
-echo -e "${GREEN}Skrip selesai! Konfigurasi berhasil diterapkan.${RESET}"
+# Aktifkan IP Forwarding
+sudo sed -i '/^#net.ipv4.ip_forward=1/s/^#//' /etc/sysctl.conf
+sudo sysctl -p
+
+# NAT Masquerading
+iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
+sudo apt install iptables-persistent -y
+
+# Instal SSH Server
+sudo apt install openssh-server -y
+sudo systemctl enable ssh
+sudo systemctl restart ssh
+
+# Buka Port SSH di Firewall
+sudo ufw allow ssh
+sudo ufw enable
+
+# Instalasi Tambahan untuk Netmiko
+sudo apt install python3 python3-pip -y
+pip3 install netmiko
+
+echo "Konfigurasi Ubuntu selesai. DHCP Server, SSH, dan NAT siap digunakan."
